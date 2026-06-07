dfab5b46014177b24a154a18c72590e9103be28a
Ayushman Gupta <ayushmangupta@Ayushmans-MacBook-Air.local>
2026-06-07 23:02:48 +0530

    Eureka and API gateway implemented

--- orderservice/src/main/java/com/example/orderservice/service/UserClient.java
+++ orderservice/src/main/java/com/example/orderservice/service/UserClient.java
@@ -6,7 +6,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "userservice",url="http://localhost:8080/")
+//@FeignClient(name = "userservice",url="http://localhost:8080/")
+@FeignClient(name="userservice")
 public interface UserClient {
     @PostMapping("api/finduser")
     UserExistResponsedto userExist (@RequestBody UserExistRequestdto userExistRequestdto);
