ec93b920eb8ed0b5dc9954b805ba08d2e0592de7
Ayushman Gupta <ayushmangupta@Ayushmans-MacBook-Air.local>
2026-06-11 21:20:12 +0530

    Kafka property added in notification service application.propertoed

---
+++ notificationservice/src/main/java/com/example/notificationservice/service/NotificationService.java
@@ -0,0 +1,18 @@
+package com.example.notificationservice.service;
+
+import com.example.notificationservice.OrderEventDTO.OrderEventDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class NotificationService {
+    @KafkaListener(topics="order-placed",
+            groupId="notification-group")
+    public void handleOrderPlaced(OrderEventDTO evt) {
+        log.info("EMAIL → User {} | Order {} | ₹{}",
+                evt.getUserId(), evt.getOrderId(), evt.getAmount());
+        // in real app: emailService.send(...)
+    }
+}
